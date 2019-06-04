1d5f8b9b15ede9b70d56abbe47f34d68d94c98df
asuleyman2403 <asuleyman2403@gmail.com>
2019-06-04 14:20:09 +0600

    asdf

--- frontend/src/app/components/SigIn/SignIn.js
+++ frontend/src/app/components/SigIn/SignIn.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import { Route, Link } from "react-router-dom";
+import './SignIn.css';
 
 class SignIn extends Component {
     render() {
         return (
             <div className="signIn">
-                <nav>
+                <nav className="signIn-nav">
                     <Link className = "nav__link" to="/">Home</Link>
                     <Link className = "nav__link" to="/register">Sign Up</Link>
                 </nav>
-                <div className="sigIn-main">
+                <div className="signIn-main">
                     <div className="signIn-center">
-                        <h1>Login to DARTwitt</h1>
+                        <h1>Login</h1>
                         <input type="text" placeholder="Enter your username" className="sigIn__input"/>
                         <input type="password" placeholder="Enter your password" className="sigIn__input"/>
                         <button className="sigIn__button">Sign In</button>
